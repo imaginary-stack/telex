2c5b8c1c7896f697fedf772ff87524596e917a7d
imaginary-stack <sx_star@pm.me>
2023-01-31 20:22:51 +0800

    Refactoring

--- src/network.rs
+++ src/network.rs
@@ -1,6 +1,8 @@
 pub const PORT: u16 = 9000;
 
-use std::net::UdpSocket;
+use crate::message::Message;
+use std::io::{Error, Read, Write};
+use std::net::{TcpListener, TcpStream, UdpSocket};
 
 pub fn get_local_ipaddr() -> String {
     let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
@@ -14,59 +16,40 @@ pub fn get_local_ipaddr() -> String {
     }
 }
 
-pub mod receive {
+pub fn listener() {
+    let listener = TcpListener::bind(get_local_ipaddr()).unwrap();
 
-    use std::{
-        fmt::Error,
-        io::Read,
-        net::{TcpListener, TcpStream},
-    };
-
-    use crate::message::Message;
-
-    use super::get_local_ipaddr;
-
-    pub fn listener() {
-        let listener = TcpListener::bind(get_local_ipaddr()).unwrap();
-
-        for stream in listener.incoming() {
-            match stream {
-                Ok(msg) => {
-                    let msg = read_string(msg).unwrap();
-                    let data: Message = Message::to_struct(msg);
-                    println!("{}: {}", data.from, data.content);
-                }
-                Err(_) => panic!(),
+    for stream in listener.incoming() {
+        match stream {
+            Ok(msg) => {
+                let msg = read_string(msg).unwrap();
+                let data: Message = Message::to_struct(msg);
+                println!("{}: {}", data.from, data.content);
             }
+            Err(_) => panic!(),
         }
-    }
-
-    fn read_string(mut msg: TcpStream) -> Result<String, Error> {
-        let mut buffer = Vec::new();
-        msg.read_to_end(&mut buffer).unwrap();
-        let message = String::from_utf8(buffer.to_vec()).unwrap();
-        return Ok(message);
     }
 }
 
-pub mod send {
-    use std::{io::Write, net::TcpStream};
+fn read_string(mut msg: TcpStream) -> Result<String, Error> {
+    let mut buffer = Vec::new();
+    msg.read_to_end(&mut buffer).unwrap();
+    let message = String::from_utf8(buffer.to_vec()).unwrap();
+    return Ok(message);
+}
 
-    use crate::message::Message;
-
-    pub fn send() {
-        let binding = Message::get_messages();
-        let msg = Message::new(binding);
-        let mut stream = TcpStream::connect("10.1.1.16:9000").unwrap();
-        stream.write_all(msg.to_json().as_bytes()).unwrap();
-    }
+pub fn send() {
+    let binding = Message::get_messages();
+    let msg = Message::new(binding);
+    let mut stream = TcpStream::connect("10.1.1.16:9000").unwrap();
+    stream.write_all(msg.to_json().as_bytes()).unwrap();
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{network::get_local_ipaddr, network::receive::listener};
+    use crate::network::{get_local_ipaddr, listener};
 
-    use super::send::send;
+    use super::send;
 
     #[test]
     fn test_get_local_ipaddr() {
